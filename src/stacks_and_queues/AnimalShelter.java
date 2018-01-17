b07ba3792637e7be7a0a62521994ba28d349378c
Rafi Barash <rafabarash@gmail.com>
2018-01-16 18:56:52 -0600

    cleaned up syntax, only minor mistakes

--- src/stacks_and_queues/AnimalShelter.java
+++ src/stacks_and_queues/AnimalShelter.java
@@ -3,39 +3,49 @@ package stacks_and_queues;
 import java.util.*;
 
 public class AnimalShelter {
-  List<Dog> dogList = new LinkedList<>();
-  List<Cat> catList = new LinkedList<>();
-  List<Animal> fullList = new LinkedList<>();
-  
-  public void enqueue(Animal animal) {
-    if (animal instanceof Dog) {
-      dogList.add(animal);
+    LinkedList<Dog> dogList = new LinkedList<>();
+    LinkedList<Cat> catList = new LinkedList<>();
+    LinkedList<Animal> fullList = new LinkedList<>();
+
+    public void enqueue(Animal animal) {
+        if (animal instanceof Dog) {
+            Dog dog = (Dog) animal;
+            dogList.add(dog);
+        } else if (animal instanceof Cat) {
+            Cat cat = (Cat) animal;
+            catList.add(cat);
+        }
+        fullList.add(animal);
     }
-    else if (animal instanceOf Cat) {
-      catList.add(animal);
+
+    public Animal dequeueAny() {
+        Animal animal = fullList.poll();
+        if (animal instanceof Dog) {
+            dogList.poll();
+        } else {
+            catList.poll();
+        }
+        return animal;
     }
-    fullList.add(animal);
-  }
-  
-  public Animal dequeueAny() {
-    Animal animal = fullList.poll();
-    if (animal instanceof Dog) {
-      dogList.poll();
-    } else {
-      catList.poll();
+
+    public Dog dequeueDog() {
+        Dog dog = dogList.poll();
+        fullList.remove(dog);
+        return dog;
     }
-    return animal;
-  }
-  
-  public Dog dequeueDog() {
-    Dog dog = dogList.poll();
-    fullList.remove(dog);
-    return dog;
-  }
-  
-  public Cat dequeueCat() {
-    Cat cat = catList.poll();
-    fullList.remove(cat);
-    return cat;
-  }
+
+    public Cat dequeueCat() {
+        Cat cat = catList.poll();
+        fullList.remove(cat);
+        return cat;
+    }
+
+    private class Animal {
+    }
+
+    private class Dog extends Animal {
+    }
+
+    private class Cat extends Animal {
+    }
 }
