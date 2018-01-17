9648ca40c7ffbaa26730e85e747e59502a69804d
Rafi Barash <rafabarash@gmail.com>
2018-01-16 18:50:49 -0600

    first solve

--- src/stacks_and_queues/AnimalShelter.java
+++ src/stacks_and_queues/AnimalShelter.java
@@ -1,4 +1,41 @@
 package stacks_and_queues;
 
+import java.util.*;
+
 public class AnimalShelter {
+  List<Dog> dogList = new LinkedList<>();
+  List<Cat> catList = new LinkedList<>();
+  List<Animal> fullList = new LinkedList<>();
+  
+  public void enqueue(Animal animal) {
+    if (animal instanceof Dog) {
+      dogList.add(animal);
+    }
+    else if (animal instanceOf Cat) {
+      catList.add(animal);
+    }
+    fullList.add(animal);
+  }
+  
+  public Animal dequeueAny() {
+    Animal animal = fullList.poll();
+    if (animal instanceof Dog) {
+      dogList.poll();
+    } else {
+      catList.poll();
+    }
+    return animal;
+  }
+  
+  public Dog dequeueDog() {
+    Dog dog = dogList.poll();
+    fullList.remove(dog);
+    return dog;
+  }
+  
+  public Cat dequeueCat() {
+    Cat cat = catList.poll();
+    fullList.remove(cat);
+    return cat;
+  }
 }
